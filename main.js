3237160b3a7a11466b1aca6f546ee1d31f2eb4d6
shouvik <shouviksinha901@gmail.com>
2022-05-10 22:31:45 +0530

    Different size grid set

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
-let 
-let gridSize = 5;
-let grid = document.createElement('div');
-grid.className = "grid";
-createGrid();
-function createGrid(){
-for(let i=0;i<gridSize;i++){
-    for(let j = 0; j<gridSize;j++){
-        document.getElementById("rightContainer").appendChild(grid);  
-        console.log(i,j);
-    }
+
+let eachGrid = document.getElementById('rightContainer');
+document.getElementById('small').addEventListener('click', createGrid(700));
+document.getElementById('medium').addEventListener('click', createGrid(30));
+document.getElementById('big').addEventListener('click', createGrid(20));
+
+//the matrix of size grid Size 
+function createGrid(x){
+for(let i=0;i<x;i++){
+        let grid = document.createElement('div');
+        grid.className = "grid small"
+        eachGrid.appendChild(grid);
+   }
 }
-}
+
+
